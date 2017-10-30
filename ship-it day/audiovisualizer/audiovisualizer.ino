3afa264293b732d699fcdbbc78e7924ef2ea2649
Edwin Hughes <edwin.hughes@macbook-pro.sds.sybrondental.com>
2017-10-30 11:29:47 -0400

    added struct for ema values

--- ship-it day/audiovisualizer/audiovisualizer.ino
+++ ship-it day/audiovisualizer/audiovisualizer.ino
@@ -8,20 +8,26 @@
 
 LedControl dotMatrix = LedControl(12,11,10,4);
 
-int sensorPin = 3;      //pin number to use the ADC
-float EMA_A = 0.4;      //initialization of EMA alpha
-int EMA_S = 0;          //initialization of EMA S
+typedef struct 
+{
+  int sensorPin;      //pin number to use the ADC
+  float a;      //initialization of EMA alpha
+  int s;          //initialization of EMA S
+} EMA;
+
+EMA ema_2 = {2, 1, 0};
+EMA ema_3 = {3, 1, 0};
     
 void setup() {
   // put your setup code here, to run once:
   for(int x=0;x<4;x++){
 dotMatrix.shutdown(x, false);
-dotMatrix.setIntensity(x, 8);
+dotMatrix.setIntensity(x, 1);
 dotMatrix.clearDisplay(x);
 }
 
-EMA_S = analogRead(sensorPin);  //set EMA S for t=1
-
+ema_2.s = analogRead(ema_2.sensorPin);  //set EMA S for t=1
+ema_3.s = analogRead(ema_3.sensorPin);  //set EMA S for t=1
 }
 
 int lastMillis = 0;
@@ -30,10 +36,11 @@ void loop() {
 
 int a0 = analogRead(0);
 int a1 = analogRead(1);
-int a2 = analogRead(2)/128;
-int a3 = analogRead(3)/128;
+int a2 = analogRead(ema_2.sensorPin)/128;
+int a3 = analogRead(ema_3.sensorPin)/128;
 
-sample(a3, EMA_A, EMA_S);
+sample(a2, ema_2);
+sample(a3, ema_3);
 
 int millisNow = millis();
 
@@ -44,15 +51,15 @@ lastMillis = millisNow;
 dotMatrix.setRow(0,0,0xff);
 dotMatrix.setRow(1,2,0xff);
 dotMatrix.clearDisplay(2);
-dotMatrix.setRow(2,a2,0xff);
+dotMatrix.setRow(2,ema_2.s,0xff);
 dotMatrix.clearDisplay(3);
-dotMatrix.setRow(3,EMA_S,0xff);
+dotMatrix.setRow(3,ema_3.s,0xff);
 }
 
 //Exponential Moving Average
 //https://www.norwegiancreations.com/2015/10/tutorial-potentiometers-with-arduino-and-filtering/
-void sample(int sensorValue, float ema_a, int& ema_s){
-  ema_s = (ema_a * sensorValue) + ((1-ema_a)*ema_s);    //run the EMA
+void sample(int sensorValue, EMA& ema){
+  ema.s = (ema.a * sensorValue) + ((1-ema.a)*ema.s);    //run the EMA
 }
 
     
