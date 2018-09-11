67c7170f03327a3188670f5722120c5ae8d786aa
Edwin Hughes <>
2018-09-10 20:35:59 -0400

    Adding simple IR remote functionality

--- demo-projects/timeandtemp/timeandtemp.ino
+++ demo-projects/timeandtemp/timeandtemp.ino
@@ -6,6 +6,38 @@
 #include <RTClib.h>
 #include <SparkFunBME280.h>
 
+#include <IRremote.h>
+
+#define LEFT 0xFF22DD
+#define RIGHT 0xFFC23D
+#define UP 0xFF629D
+#define DOWN 0xFFA857
+#define OK 0xFF02FD
+
+#define ONE 0xFF6897
+#define TWO 0xFF9867
+#define THREE 0xFFB04F
+#define FOUR 0xFF30CF
+#define FIVE 0xFF18E7
+#define SIX 0xFF7A85
+#define SEVEN 0xFF10EF
+#define EIGHT 0xFF38C7
+#define NINE 0xFF5AA5
+#define ZERO 0xFF4AB5
+
+#define STAR 0xFF42BD
+#define HASH 0xFF52AD
+
+#define REPEAT_KEY 0xFFFFFFFF
+
+int RECV_PIN = 7;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+int key = ZERO;
+int textSize = 2;
+
 #define OLED_RESET 4
 
 Adafruit_SSD1306 display(OLED_RESET);
@@ -34,6 +66,110 @@ void setup () {
   bme280.settings.pressOverSample = 1;
   bme280.settings.humidOverSample = 1;
   bme280.begin();
+
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void processKeyPress(int key)
+{
+  switch(key)
+    {
+      case ONE: 
+      {
+        textSize=1;
+        Serial.println("ONE");
+        break;
+      }
+      case TWO:
+      {
+        textSize=2;
+        Serial.println("TWO");
+        break;
+      }
+      case THREE:
+      {
+        textSize=3;
+        Serial.println("THREE");
+        break;
+      }
+      case FOUR: 
+      {
+        textSize=4;
+        Serial.println("FOUR");
+        break;
+      }
+      case FIVE:
+      {
+        Serial.println("FIVE");
+        break;
+      }
+      case SIX:
+      {
+        Serial.println("SIX");
+        break;
+      }
+      case SEVEN:
+      {
+        Serial.println("SEVEN");
+        break;
+      }
+      case EIGHT:
+      {
+        Serial.println("EIGHT");
+        break;
+      }
+      case NINE:
+      {
+        Serial.println("NINE");
+        break;
+      }
+      case ZERO:
+      {
+        Serial.println("ZERO");
+        break;
+      }
+      case UP:
+      {
+        Serial.println("UP");
+        break;
+      }
+      case DOWN:
+      {
+        Serial.println("DOWN");
+        break;
+      }
+      case OK:
+      {
+        Serial.println("OK");
+        break;
+      }
+      case STAR:
+      {
+        Serial.println("STAR");
+        break;
+      }
+      case HASH:
+      {
+        Serial.println("HASH");
+        break;
+      }
+      case LEFT:
+      {
+        Serial.println("LEFT");
+        break;
+      }
+      case RIGHT:
+      {
+        Serial.println("RIGHT");
+        break;
+      }
+      case REPEAT_KEY:
+      {
+        Serial.println("REPEAT_KEY");
+        break;
+      }
+    }      
 }
 
 void loop () {
@@ -41,7 +177,7 @@ void loop () {
 
 	display.clearDisplay();
   
-  display.setTextSize(3);
+  display.setTextSize(textSize);
 	display.setTextColor(WHITE);
 	display.setCursor(0,0);
 
@@ -52,9 +188,11 @@ void loop () {
   //display.print(':');
   display.setTextSize(1);
   printNumber(now.second());
-  display.println();
-  display.println();
-  display.println();
+  
+  for(int x = textSize; x > 0; x--)
+  {
+    display.println();
+  }
 
   display.print(daysOfTheWeek[now.dayOfTheWeek()]);
 
@@ -66,30 +204,40 @@ void loop () {
   printNumber(now.day());
   display.println();
 
-  display.setTextSize(2);
+  display.setTextSize(1);
   //display.print(bme280.readTempC(), 2);
   //display.print("C, ");
 
-  display.print(bme280.readTempF(), 2);
+  display.print(bme280.readTempF(), 1);
   display.setTextSize(1);
   display.print("  o");
-  display.setTextSize(2);
-  display.println("F");
+  display.setTextSize(1);
+  display.print("F ");
 
-  display.print(bme280.readFloatHumidity(), 2);
+  display.print(bme280.readFloatHumidity(), 1);
   display.println(" %RH");
   
-  //display.print(bme280.readFloatPressure(), 2);
-  //display.println(" Pa");
+  display.print(bme280.readFloatPressure()/1000, 2);
+  display.print(" KPa, ");
 
-  //display.print(bme280.readFloatAltitudeMeters(), 2);
-  //display.print("m, ");
+  display.print(bme280.readFloatAltitudeMeters(), 2);
+  display.print("m, ");
 
-  //display.print(bme280.readFloatAltitudeFeet(), 2);
-  //display.println("ft"); 
+  display.print(bme280.readFloatAltitudeFeet(), 2);
+  display.println("ft"); 
 
   display.display();
   delay(125);
+
+  if (irrecv.decode(&results)) {
+
+    if (results.value != REPEAT_KEY)
+    {
+      key = results.value;
+    }
+    processKeyPress(key);
+    irrecv.resume();
+  }
 }
 
 void printNumber(int number)
