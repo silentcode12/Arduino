22ec79c8d588dfa56278a72a08614559a786c067
eddie_england@hotmail.com <eddie_england@hotmail.com>
2020-07-22 19:36:42 -0400

    Removed IRremote library Add some basic filtering of samples to reduce noise on output Added some basic formatting

--- demo-projects/timeandtemp/timeandtemp.ino
+++ demo-projects/timeandtemp/timeandtemp.ino
@@ -1,56 +1,43 @@
-// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <RTClib.h>
 #include <SparkFunBME280.h>
 
-#include <IRremote.h>
-
-#define LEFT 0xFF22DD
-#define RIGHT 0xFFC23D
-#define UP 0xFF629D
-#define DOWN 0xFFA857
-#define OK 0xFF02FD
-
-#define ONE 0xFF6897
-#define TWO 0xFF9867
-#define THREE 0xFFB04F
-#define FOUR 0xFF30CF
-#define FIVE 0xFF18E7
-#define SIX 0xFF7A85
-#define SEVEN 0xFF10EF
-#define EIGHT 0xFF38C7
-#define NINE 0xFF5AA5
-#define ZERO 0xFF4AB5
-
-#define STAR 0xFF42BD
-#define HASH 0xFF52AD
-
-#define REPEAT_KEY 0xFFFFFFFF
-
-int RECV_PIN = 7;
-
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
-int key = ZERO;
-volatile int textSize = 2;
+volatile int textSize = 3;
 
 #define OLED_RESET 4
+#define BUTTON_PIN 2
 
 Adafruit_SSD1306 display(OLED_RESET);
 RTC_DS3231 rtc;
 BME280 bme280;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+typedef struct 
+{
+  float a;      //initialization of EMA alpha
+  float s;      //initialization of EMA S
+} EMA;
+
+EMA percentRH {0.1, 0};
+EMA tempF {0.9, 0};
+EMA altMeters {0.1, 0};
+EMA altFeet {0.1, 0};
+EMA preasureKpa {0.1, 0};
+
+//Exponential Moving Average
+//https://www.norwegiancreations.com/2015/10/tutorial-potentiometers-with-arduino-and-filtering/
+void UpdateEma(EMA& ema, float sample)
+{
+  ema.s = (ema.a * sample) + ((1-ema.a)*ema.s);    //run the EMA
+}
 
 void setup () {
   if (! rtc.begin()) {
     while (1);
   }
 
+  //No way to set time other than to uncomment this and upload to device.  Then uncomment and re-upload.
   //if (rtc.lostPower()) {
    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   //}
@@ -68,19 +55,24 @@ void setup () {
   bme280.begin();
 
   Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
 
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  
-  attachInterrupt(digitalPinToInterrupt(2), pin2ISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(3), pin3ISR, FALLING);
+  pinMode(4, INPUT);         //Square wave from rtc, useless as no interop on nano
+  pinMode(5, INPUT);         //32K from rtc, useless as no interop on nano
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  //Button pin
+
+  //Prime the first value to avoid initial homing of average from zero.
+  percentRH.s = bme280.readFloatHumidity();
+  tempF.s = bme280.readTempF();
+  altFeet.s = bme280.readFloatAltitudeFeet();
+  preasureKpa.s = bme280.readFloatPressure();
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), pin2ISR, FALLING);
 }
 
 volatile long lastFall = 0;
 
 void pin2ISR(){
-  volatile int currentValue = digitalRead(2);
+  volatile int currentValue = digitalRead(BUTTON_PIN);
 
   if (currentValue == HIGH)
     return;  //We were falling but now we're high
@@ -93,7 +85,7 @@ void pin2ISR(){
   }
 
 lastFall = now;
-currentValue = digitalRead(2);
+currentValue = digitalRead(BUTTON_PIN);
 if (currentValue == HIGH)
     return;  //We were falling but now we're high
 
@@ -105,120 +97,38 @@ if (currentValue == HIGH)
     textSize++;
 }
 
-void pin3ISR(){
-  Serial.println("pin3ISR");
-}
-
-void processKeyPress(int key)
-{
-  switch(key)
-    {
-      case ONE: 
-      {
-        textSize=1;
-        Serial.println("ONE");
-        break;
-      }
-      case TWO:
-      {
-        textSize=2;
-        Serial.println("TWO");
-        break;
-      }
-      case THREE:
-      {
-        textSize=3;
-        Serial.println("THREE");
-        break;
-      }
-      case FOUR: 
-      {
-        textSize=4;
-        Serial.println("FOUR");
-        break;
-      }
-      case FIVE:
-      {
-        Serial.println("FIVE");
-        break;
-      }
-      case SIX:
-      {
-        Serial.println("SIX");
-        break;
-      }
-      case SEVEN:
-      {
-        Serial.println("SEVEN");
-        break;
-      }
-      case EIGHT:
-      {
-        Serial.println("EIGHT");
-        break;
-      }
-      case NINE:
-      {
-        Serial.println("NINE");
-        break;
-      }
-      case ZERO:
-      {
-        Serial.println("ZERO");
-        break;
-      }
-      case UP:
-      {
-        Serial.println("UP");
-        break;
-      }
-      case DOWN:
-      {
-        Serial.println("DOWN");
-        break;
-      }
-      case OK:
-      {
-        Serial.println("OK");
-        break;
-      }
-      case STAR:
-      {
-        Serial.println("STAR");
-        break;
-      }
-      case HASH:
-      {
-        Serial.println("HASH");
-        break;
-      }
-      case LEFT:
-      {
-        Serial.println("LEFT");
-        break;
-      }
-      case RIGHT:
-      {
-        Serial.println("RIGHT");
-        break;
-      }
-      case REPEAT_KEY:
-      {
-        Serial.println("REPEAT_KEY");
-        break;
-      }
-    }      
-}
-
 void loop () {
   DateTime now = rtc.now();
 
 	display.clearDisplay();
-  
-  display.setTextSize(textSize);
 	display.setTextColor(WHITE);
 	display.setCursor(0,0);
 
+  if (textSize == 4)
+  {
+    display.setTextSize(1);
+    display.println();
+    display.println();
+  }
+  else if (textSize == 3)
+  {
+    display.setTextSize(1);
+    display.println();
+  }
+   else if (textSize == 2)
+  {
+    display.setTextSize(1);
+    display.println();
+  }
+   else if (textSize == 1)
+  {
+    display.setTextSize(1);
+    display.println();
+    display.println();
+    display.println();
+  }
+  
+  display.setTextSize(textSize);
   display.print("");
   printNumber(now.hour());
   display.print(':');
@@ -250,40 +160,33 @@ void loop () {
   //display.print(bme280.readTempC(), 2);
   //display.print("C, ");
 
-  display.print(bme280.readTempF(), 1);
+  UpdateEma(tempF, bme280.readTempF() - 10.0);
+  display.print(tempF.s, 1);
   display.setTextSize(1);
   display.print("  o");
   display.setTextSize(1);
   display.print("F ");
 
-  display.print(bme280.readFloatHumidity(), 1);
+  UpdateEma(percentRH, bme280.readFloatHumidity());
+  display.print(percentRH.s, 1);
   display.println(" %RH");
-  
-  display.print(bme280.readFloatPressure()/1000, 2);
+
+  UpdateEma(preasureKpa, bme280.readFloatPressure());
+  display.print(preasureKpa.s/1000.0, 2);
   display.print(" KPa, ");
 
-  display.print(bme280.readFloatAltitudeMeters(), 2);
-  display.print("m, ");
-
-  display.print(bme280.readFloatAltitudeFeet(), 2);
-  display.println("ft"); 
+  UpdateEma(altFeet, bme280.readFloatAltitudeFeet());
+  display.print(altFeet.s, 2);
+  display.println(" ft"); 
 
   display.display();
-  delay(125);
-
-  if (irrecv.decode(&results)) {
-
-    if (results.value != REPEAT_KEY)
-    {
-      key = results.value;
-    }
-    processKeyPress(key);
-    irrecv.resume();
-  }
+  delay(250);
 }
 
 void printNumber(int number)
 {
-  if (number < 10) display.print("0");
+  if (number < 10) 
+    display.print("0");
+    
   display.print(number, DEC);
 }
